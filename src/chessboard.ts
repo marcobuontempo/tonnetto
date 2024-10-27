7bab2bf7dd029d3b90965859a74c52c603fe305d
marcobuontempo <marco.buon36@gmail.com>
2024-10-27 18:29:03 +1100

    Complete pseudo legal move generation

--- src/chessboard.ts
+++ src/chessboard.ts
@@ -1,7 +1,9 @@
-import { EMPTY, EDGE, PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, DEFAULT_FEN, WHITE, BLACK, PIECE_LOOKUP, SQUARE_ASCII, MAILBOX120, MAILBOX64, HAS_MOVED, PIECE_MASK, COLOUR_MASK } from "./constants";
+import { BOARD_STATES, DEFAULT_FEN, MAILBOX64, PIECE, PIECE_LOOKUP, PIECE_MASK, SQUARE, SQUARE_ASCII } from "./constants";
 
 export default class ChessBoard {
-  public board = new Uint8Array(120).fill(EDGE);  // 10x12 1D-array board representation
+  public board = new Uint8Array(120).fill(SQUARE.EDGE);  // 10x12 1D-array board representation
+  public state = new Uint32Array(3600).fill(BOARD_STATES.EMPTY);
+  public ply = 0;
 
   constructor(fen = DEFAULT_FEN) {
     this.parseFen(fen);
@@ -9,8 +11,9 @@ export default class ChessBoard {
 
   parseFen(fen: string) {
     // populate 8x8 board
-    const board64 = new Uint8Array(64).fill(EMPTY);
-    const piecesFEN = fen.split(" ")[0].replace(/\//g, "");
+    const board64 = new Uint8Array(64).fill(SQUARE.EMPTY);
+    const [boardFEN, turnFEN, castleFEN, epFEN, halfmoveFEN, fullmoveFEN] = fen.split(" ");
+    const piecesFEN = boardFEN.replace(/\//g, "");
 
     let boardIndex = 0;
     for (let i = 0; i < piecesFEN.length; i++) {
@@ -19,13 +22,13 @@ export default class ChessBoard {
         boardIndex += parseInt(pieceFEN);
       }
       else {
-        const colour = pieceFEN.toUpperCase() === pieceFEN ? WHITE : BLACK;
+        const colour = pieceFEN.toUpperCase() === pieceFEN ? PIECE.IS_WHITE : PIECE.IS_BLACK;
         const piece = PIECE_LOOKUP[pieceFEN.toUpperCase()];
-        let hasMoved = HAS_MOVED;
-        if (piece === PAWN) {
+        let hasMoved = PIECE.HAS_MOVED;
+        if (piece === PIECE.PAWN) {
           if (
-            (colour === WHITE && boardIndex >= 48 && boardIndex <= 55) ||
-            (colour === BLACK && boardIndex >= 8 && boardIndex <= 15)
+            (colour === PIECE.IS_WHITE && boardIndex >= 48 && boardIndex <= 55) ||
+            (colour === PIECE.IS_BLACK && boardIndex >= 8 && boardIndex <= 15)
           ) {
             hasMoved = 0;
           }
@@ -41,6 +44,34 @@ export default class ChessBoard {
       const newIdx = MAILBOX64[i];
       this.board[newIdx] = board64[i];
     }
+
+    const currentTurn = turnFEN === 'w' ? BOARD_STATES.CURRENT_TURN_WHITE : BOARD_STATES.CURRENT_TURN_BLACK;
+
+    let castleRights = BOARD_STATES.EMPTY;
+    if (castleFEN.includes('K')) castleRights |= BOARD_STATES.WHITE_KINGSIDE_CASTLE;
+    if (castleFEN.includes('Q')) castleRights |= BOARD_STATES.WHITE_QUEENSIDE_CASTLE;
+    if (castleFEN.includes('k')) castleRights |= BOARD_STATES.BLACK_KINGSIDE_CASTLE;
+    if (castleFEN.includes('q')) castleRights |= BOARD_STATES.BLACK_QUEENSIDE_CASTLE;
+
+    const enPassantTarget = epFEN === "-" ? 0 : this.algebraicToIndex(epFEN) << 8;
+
+    const halfmove = parseInt(halfmoveFEN);
+
+    const fullmove = parseInt(fullmoveFEN);
+
+    const state = BOARD_STATES.EMPTY | currentTurn | castleRights | enPassantTarget | halfmove;
+
+    const ply = 2 * (fullmove - 1) + (turnFEN === 'w' ? 1 : 2);
+
+    this.ply = ply;
+    this.state[ply] = state;
+  }
+
+  algebraicToIndex(notation: string): number {
+    const [fileCh, rankCh] = notation.split("");
+    const file = fileCh.charCodeAt(0) - 97;
+    const rank = 64 - parseInt(rankCh) * 8;
+    return MAILBOX64[file + rank];
   }
 
   printBoard() {
@@ -54,8 +85,8 @@ export default class ChessBoard {
       }
 
       const square = this.board[idx];
-      const piece = square & PIECE_MASK;
-      const colour = square & COLOUR_MASK;
+      const piece = square & PIECE_MASK.TYPE;
+      const colour = square & PIECE_MASK.COLOUR;
       output += SQUARE_ASCII[piece | colour];
     }
 
--- src/engine.ts
+++ src/engine.ts
@@ -1,5 +1,5 @@
 import ChessBoard from "./chessboard";
-import { EMPTY, EDGE, PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, DEFAULT_FEN, WHITE, BLACK, PIECE_MASK, COLOUR_MASK, MAILBOX64, MAILBOX120, MOVE_LIST, SQUARE_ASCII, SLIDERS, HAS_MOVED } from "./constants";
+import { BOARD_STATES, DIRECTION, ENCODED_MOVE, MAILBOX64, CASTLE_INDEXES, MOVE_LIST, PIECE, PIECE_MASK, PROMOTION_PIECES, SLIDERS, SQUARE } from "./constants";
 
 export default class Engine {
   private chessboard: ChessBoard;
@@ -10,42 +10,44 @@ export default class Engine {
   }
 
   generatePseudoMoves(turnColour: number) {
-    const moves = new Uint32Array(3600);
+    const moves = new Uint32Array(256);
     let moveIdx = 0;
+
+    const ply = this.chessboard.ply;
+    const state = this.chessboard.state[ply];
 
     for (let i = 0; i < 64; i++) {
       const fromBoardIndex = MAILBOX64[i];
       const fromSquare = this.chessboard.board[fromBoardIndex];
-      const fromPiece = fromSquare & PIECE_MASK;
-      const fromColour = fromSquare & COLOUR_MASK;
-      const fromHasMoved = fromSquare & HAS_MOVED ? 1 : 0;
+      const fromPiece = fromSquare & PIECE_MASK.TYPE;
+      const fromColour = fromSquare & PIECE_MASK.COLOUR;
+      const fromHasMoved = fromSquare & PIECE.HAS_MOVED ? 1 : 0;
 
       // skip checking pieces from opposition colour
       if (fromColour !== turnColour) continue;
       
       // get index for MOVE_LIST and SLIDERS (index is their piece number, except black pawn is 0 as it moves different to white pawn)
-      const fromPieceIndex = (fromPiece === PAWN && fromColour === BLACK) ? 0 : fromPiece;
+      const fromPieceIndex = (fromPiece === PIECE.PAWN && fromColour === PIECE.IS_BLACK) ? 0 : fromPiece;
 
       const directions = MOVE_LIST[fromPieceIndex];
       const slider = SLIDERS[fromPieceIndex];
  
       for (let direction of directions) {
-        let toBoardIndex = fromBoardIndex;
+        let toBoardIndex = fromBoardIndex;  // initialise from the current square, and we will add directions to it next during generation
 
         while (true) {
+          if (fromPiece === PIECE.PAWN) break;  // handle all pawn moves separately, as they contain special rules
+
           toBoardIndex += direction;
 
           const toSquare = this.chessboard.board[toBoardIndex];
-          const toColour = toSquare & COLOUR_MASK;
+          const toColour = toSquare & PIECE_MASK.COLOUR;
 
-          let move = 0;
-
-          if (toSquare === EMPTY) {
-            // empty square means piece can move here
-            move = (fromHasMoved << 24) | (toBoardIndex << 8) | (fromBoardIndex);
-            moves[moveIdx++] = move;
+          if (toSquare === SQUARE.EMPTY) {
+            // empty square means piece can move here (pawn moves handled different due to promotions)
+            moves[moveIdx++] = (fromHasMoved << 24) | (toBoardIndex << 8) | (fromBoardIndex);
           }
-          else if (toSquare === EDGE) {
+          else if (toSquare === SQUARE.EDGE) {
             // if edge of board, stop search in this direction
             break;
           }
@@ -53,36 +55,120 @@ export default class Engine {
             // if blocked by same colour pieces, stop search in this direction
             break;
           }
-          else if (toColour !== fromColour && fromPiece !== PAWN) {
-            // if opponent piece, capture (except pawn, cannot capture with forward push)
-            move = (fromHasMoved << 24) | (toSquare << 16) | (toBoardIndex << 8) | (fromBoardIndex);
-            moves[moveIdx++] = move;
+          else if (toColour !== fromColour) {
+            // if opponent piece, capture
+            moves[moveIdx++] = (fromHasMoved << 24) | (toSquare << 16) | (toBoardIndex << 8) | (fromBoardIndex);
           }
           
           if (slider === false) {
             break;
           }
         }
-
-        if (fromPiece === PAWN) break;  // only single pawn push here - special moves are generated later
       }
 
 
       /* SPECIAL MOVES */
 
       // pawn
-      if (fromPiece === PAWN) {
-        // check direction0
-          // if empty, add move
-          // check !has_moved, if so, check direction1
-            // if also empty, add move
+      if (fromPiece === PIECE.PAWN) {
+        // get all move positions
+        const singlePushIndex = fromBoardIndex + directions[0];
+        const doublePushIndex = fromBoardIndex + directions[1];
+        const eastDiagonalIndex = fromBoardIndex + directions[2];
+        const westDiagonalIndex = fromBoardIndex + directions[3];
 
-        // check direction 2 & 3
-          // if opposite piece, add capture
-          // if en passant square, add capture
+        // get en passant target index, and the actual target pawn if so 
+        const enPassantTarget = (state & BOARD_STATES.EN_PASSANT_SQUARE) >> 8;
+        let enPassantSquare;
+        
+        if (enPassantTarget >= 71 && enPassantTarget <= 78) {
+          enPassantSquare = this.chessboard.board[enPassantTarget + DIRECTION.NORTH];
+        }
+        else if (enPassantTarget >= 41 && enPassantTarget <= 48) {
+          enPassantSquare = this.chessboard.board[enPassantTarget + DIRECTION.SOUTH];
+        }
+        
+        if (this.chessboard.board[singlePushIndex] === SQUARE.EMPTY) {
+          if (singlePushIndex >= 31 && singlePushIndex <= 81) {
+            // regular single push
+            moves[moveIdx++] = (fromHasMoved << 24) | (singlePushIndex << 8) | (fromBoardIndex);
+          }
+          else {
+            // single push with promotion
+            for (let promotionPiece of PROMOTION_PIECES) {
+              moves[moveIdx++] = (promotionPiece << 25) | (fromHasMoved << 24) | (singlePushIndex << 8) | (fromBoardIndex);
+            }
+          }
+
+          if (!fromHasMoved && this.chessboard.board[doublePushIndex] === SQUARE.EMPTY) {
+            // double push
+            moves[moveIdx++] = (ENCODED_MOVE.DOUBLE_PUSH_MOVE) | (fromHasMoved << 24) | (doublePushIndex << 8) | (fromBoardIndex);
+          }
+        }
+
+        const eastSquare = this.chessboard.board[eastDiagonalIndex];
+        const eastColour = eastSquare | PIECE_MASK.COLOUR;
+        if (eastColour !== fromColour) {
+          if (eastDiagonalIndex >= 31 && eastDiagonalIndex <= 81) {
+            // east diagonal piece capture
+            moves[moveIdx++] = (fromHasMoved << 24) | (eastSquare << 16) | (eastDiagonalIndex << 8) | (fromBoardIndex);
+          }
+          else {
+            // east diagonal piece capture with promotion
+            for (let promotionPiece of PROMOTION_PIECES) {
+              moves[moveIdx++] = (promotionPiece << 25) | (fromHasMoved << 24) | (eastSquare << 16) | (eastDiagonalIndex << 8) | (fromBoardIndex);
+            }
+          }
+        }
+        else if (eastDiagonalIndex === enPassantTarget && enPassantSquare) {
+          // en passant capture
+          moves[moveIdx++] = (ENCODED_MOVE.EN_PASSANT_MOVE) | (fromHasMoved << 24) | (enPassantSquare << 16) | (eastDiagonalIndex << 8) | (fromBoardIndex);
+        }
+
+        const westSquare = this.chessboard.board[westDiagonalIndex];
+        const westColour = westSquare | PIECE_MASK.COLOUR;
+        if (westColour !== fromColour) {
+          if (westDiagonalIndex >= 31 && westDiagonalIndex <= 81) {
+            // west diagonal piece capture
+            moves[moveIdx++] = (fromHasMoved << 24) | (westSquare << 16) | (westDiagonalIndex << 8) | (fromBoardIndex);
+          }
+          else {
+            // west diagonal piece capture with promotion
+            for (let promotionPiece of PROMOTION_PIECES) {
+              moves[moveIdx++] = (promotionPiece << 25) | (fromHasMoved << 24) | (westSquare << 16) | (westDiagonalIndex << 8) | (fromBoardIndex);
+            }
+          }
+        }
+        else if (westDiagonalIndex === enPassantTarget && enPassantSquare) {
+          // en passant capture
+          moves[moveIdx++] = (ENCODED_MOVE.EN_PASSANT_MOVE) | (fromHasMoved << 24) | (enPassantSquare << 16) | (westDiagonalIndex << 8) | (fromBoardIndex);
+        }
+      }
+
+      // castle
+      if (fromPiece === PIECE.KING) {
+        if (fromColour === PIECE.IS_WHITE) {
+          if (state & BOARD_STATES.WHITE_KINGSIDE_CASTLE) {
+            // add white kingside castle
+            moves[moveIdx++] = (ENCODED_MOVE.KINGSIDE_CASTLE_MOVE) | (fromHasMoved << 24) | (CASTLE_INDEXES.KING_TO_KINGSIDE << 8) | (fromBoardIndex);
+          }
+          if (state & BOARD_STATES.WHITE_QUEENSIDE_CASTLE) {
+            // add white queenside castle
+            moves[moveIdx++] = (ENCODED_MOVE.QUEENSIDE_CASTLE_MOVE) | (fromHasMoved << 24) | (CASTLE_INDEXES.KING_TO_QUEENSIDE << 8) | (fromBoardIndex);
+          }
+        }
+        else if (fromColour === PIECE.IS_BLACK) {
+          if (state & BOARD_STATES.BLACK_KINGSIDE_CASTLE) {
+            // add black kingside castle
+            moves[moveIdx++] = (ENCODED_MOVE.KINGSIDE_CASTLE_MOVE) | (fromHasMoved << 24) | (CASTLE_INDEXES.KING_TO_KINGSIDE << 8) | (fromBoardIndex);
+          }
+          if (state & BOARD_STATES.BLACK_QUEENSIDE_CASTLE) {
+            // add black queenside castle
+            moves[moveIdx++] = (ENCODED_MOVE.QUEENSIDE_CASTLE_MOVE) | (fromHasMoved << 24) | (CASTLE_INDEXES.KING_TO_QUEENSIDE << 8) | (fromBoardIndex);
+          }
+        }
       }
     }
-
   }
 }
 
