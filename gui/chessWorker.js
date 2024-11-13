8db81e03e0d4b12df4aebd029ee69bbb011d28d6
marcobuontempo <marco.buon36@gmail.com>
2024-11-13 16:37:09 +1100

    Update relative path import for chessWorker in GUI

--- gui/chessWorker.js
+++ gui/chessWorker.js
@@ -1,4 +1,4 @@
-import { ChessEngineAPI } from "../engine/dist/engine.bundle.js";
+import { ChessEngineAPI } from "./dist/engine.bundle.js";
 
 self.onmessage = function(event) {
   const message = JSON.parse(event.data);
